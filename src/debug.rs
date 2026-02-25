6aa68611c4fa10026c42f5435d14fd4921a978d1
weasel-lee <weasel@unnoted.com>
2026-02-25 09:08:30 +0000

    WIP

--- src/debug.rs
+++ src/debug.rs
@@ -67,12 +67,14 @@ pub(crate) fn utf8_decode(bytes: &[u8]) -> Option<Result<char, u8>> {
     if bytes.is_empty() {
         return None;
     }
+
     let len = match len(bytes[0]) {
         None => return Some(Err(bytes[0])),
         Some(len) if len > bytes.len() => return Some(Err(bytes[0])),
         Some(1) => return Some(Ok(char::from(bytes[0]))),
         Some(len) => len,
     };
+
     match core::str::from_utf8(&bytes[..len]) {
         Ok(s) => Some(Ok(s.chars().next().unwrap())),
         Err(_) => Some(Err(bytes[0])),
